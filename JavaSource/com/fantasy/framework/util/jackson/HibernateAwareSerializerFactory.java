a88cc60ff489dfa035645d95f4e3bbca35bfeb80
limaofeng <limaofeng@msn.com>
2014-09-20 15:39:28 +0800

    Json 框架升级，版本升级到 2.3.1

--- JavaSource/com/fantasy/framework/util/jackson/HibernateAwareSerializerFactory.java
+++ JavaSource/com/fantasy/framework/util/jackson/HibernateAwareSerializerFactory.java
@@ -3,29 +3,23 @@ package com.fantasy.framework.util.jackson;
 import com.fantasy.framework.util.jackson.serializer.DateSerializer;
 import com.fantasy.framework.util.ognl.typeConverter.DateFormat;
 import org.codehaus.jackson.JsonGenerator;
-import org.codehaus.jackson.map.BeanProperty;
-import org.codehaus.jackson.map.JsonSerializer;
-import org.codehaus.jackson.map.SerializationConfig;
-import org.codehaus.jackson.map.SerializerProvider;
+import org.codehaus.jackson.map.*;
 import org.codehaus.jackson.map.introspect.Annotated;
 import org.codehaus.jackson.map.introspect.AnnotatedMethod;
 import org.codehaus.jackson.map.introspect.BasicBeanDescription;
 import org.codehaus.jackson.map.ser.BeanPropertyWriter;
 import org.codehaus.jackson.map.ser.CustomSerializerFactory;
+import org.codehaus.jackson.map.type.CollectionLikeType;
+import org.codehaus.jackson.map.type.CollectionType;
+import org.codehaus.jackson.map.type.MapLikeType;
+import org.codehaus.jackson.map.type.MapType;
 import org.codehaus.jackson.type.JavaType;
 import org.hibernate.bytecode.internal.javassist.FieldHandled;
-import org.hibernate.collection.internal.PersistentMap;
 import org.hibernate.collection.spi.PersistentCollection;
 import org.hibernate.proxy.HibernateProxy;
 import org.hibernate.proxy.LazyInitializer;
-import org.springframework.beans.BeanUtils;
-import org.springframework.core.annotation.AnnotationUtils;
 
-import javax.persistence.Transient;
-import java.beans.PropertyDescriptor;
 import java.io.IOException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -37,7 +31,7 @@ public class HibernateAwareSerializerFactory extends CustomSerializerFactory {
 	private static final String FIELD_HANDLER_PROPERTY_NAME = "fieldHandler";
 
 	@Override
-	public JsonSerializer<Object> createSerializer(SerializationConfig config, JavaType type, BeanProperty property) /*throws JsonMappingException*/ {
+	public JsonSerializer<Object> createSerializer(SerializationConfig config, JavaType type, BeanProperty property) throws JsonMappingException /*throws JsonMappingException*/ {
 		Class<?> clazz = type.getRawClass();
 		if (PersistentCollection.class.isAssignableFrom(clazz)) {
 			return new PersistentCollectionSerializer(config, type, property);
@@ -49,7 +43,7 @@ public class HibernateAwareSerializerFactory extends CustomSerializerFactory {
 	}
 
 	@Override
-	protected JsonSerializer<Object> findSerializerFromAnnotation(SerializationConfig config, Annotated annotated, BeanProperty property) /*throws JsonMappingException*/ {
+	protected JsonSerializer<Object> findSerializerFromAnnotation(SerializationConfig config, Annotated annotated, BeanProperty property) throws JsonMappingException /*throws JsonMappingException*/ {
 		JsonSerializer<Object> serializer = super.findSerializerFromAnnotation(config, annotated, property);
 		if ((Object) serializer instanceof DateSerializer && annotated instanceof AnnotatedMethod) {
 			DateFormat dateFormat = ((AnnotatedMethod) annotated).getAnnotated().getAnnotation(DateFormat.class);
@@ -62,19 +56,19 @@ public class HibernateAwareSerializerFactory extends CustomSerializerFactory {
 	protected List<BeanPropertyWriter> filterBeanProperties(SerializationConfig config, BasicBeanDescription beanDesc, List<BeanPropertyWriter> props) {
 		props = super.filterBeanProperties(config, beanDesc, props);
 		filterInstrumentedBeanProperties(beanDesc, props);
-		List<String> transientOnes = new ArrayList<String>();
-		// 去掉瞬态字段
-		for (PropertyDescriptor pd : BeanUtils.getPropertyDescriptors(beanDesc.getBeanClass())) {
-			Method getter = pd.getReadMethod();
-			if (getter != null && AnnotationUtils.findAnnotation(getter, Transient.class) != null) {
+//		List<String> transientOnes = new ArrayList<String>();
+//		// 去掉瞬态字段
+//		for (PropertyDescriptor pd : BeanUtils.getPropertyDescriptors(beanDesc.getBeanClass())) {
+//			Method getter = pd.getReadMethod();
+//			if (getter != null && AnnotationUtils.findAnnotation(getter, Transient.class) != null) {
 //				transientOnes.add(pd.getName());
-			}
-		}
-		for (Iterator<BeanPropertyWriter> iter = props.iterator(); iter.hasNext();) {
-			if (transientOnes.contains(iter.next().getName())) {
-				iter.remove();
-			}
-		}
+//			}
+//		}
+//		for (Iterator<BeanPropertyWriter> iter = props.iterator(); iter.hasNext();) {
+//			if (transientOnes.contains(iter.next().getName())) {
+//				iter.remove();
+//			}
+//		}
 		return props;
 	}
 
@@ -108,8 +102,7 @@ public class HibernateAwareSerializerFactory extends CustomSerializerFactory {
 				return;
 			}
 			BasicBeanDescription beanDesc = config.introspect(type);
-			/*
-			boolean staticTyping = false;
+//			boolean staticTyping = false;
 			JavaType elementType = type.getContentType();
 			TypeSerializer elementTypeSerializer = createTypeSerializer(config, elementType, beanProperty);
 
@@ -120,31 +113,33 @@ public class HibernateAwareSerializerFactory extends CustomSerializerFactory {
 				MapLikeType mlt = (MapLikeType) type;
 				JsonSerializer keySerializer = findKeySerializer(config, beanDesc.getClassInfo(), beanProperty);
 				if (mlt.isTrueMapType()) {
-					serializer = (JsonSerializer<Object>) buildMapSerializer(config, (MapType) mlt, beanDesc, beanProperty, staticTyping, keySerializer, elementTypeSerializer, elementValueSerializer);
-				}
-				serializer = (JsonSerializer<Object>) buildMapLikeSerializer(config, mlt, beanDesc, beanProperty, staticTyping, keySerializer, elementTypeSerializer, elementValueSerializer);
+					serializer = (JsonSerializer<Object>) buildMapSerializer(config, (MapType) mlt, beanDesc, beanProperty, false, keySerializer, elementTypeSerializer, elementValueSerializer);
+				}else {
+                    serializer = (JsonSerializer<Object>) buildMapLikeSerializer(config, mlt, beanDesc, beanProperty, false, keySerializer, elementTypeSerializer, elementValueSerializer);
+                }
 			} else if (type.isCollectionLikeType()) {
 				CollectionLikeType clt = (CollectionLikeType) type;
 				if (clt.isTrueCollectionType()) {
-					serializer = (JsonSerializer<Object>) buildCollectionSerializer(config, (CollectionType) clt, beanDesc, beanProperty, staticTyping, elementTypeSerializer, elementValueSerializer);
+					serializer = (JsonSerializer<Object>) buildCollectionSerializer(config, (CollectionType) clt, beanDesc, beanProperty, false, elementTypeSerializer, elementValueSerializer);
 				} else {
-					serializer = (JsonSerializer<Object>) buildCollectionLikeSerializer(config, clt, beanDesc, beanProperty, staticTyping, elementTypeSerializer, elementValueSerializer);
+					serializer = (JsonSerializer<Object>) buildCollectionLikeSerializer(config, clt, beanDesc, beanProperty, false, elementTypeSerializer, elementValueSerializer);
 				}
 			}
 			if (serializer == null) {
 				throw new RuntimeException(" JsonSerializer is null !");
 			}
-			*/
-			Class<?> clazz = type.getRawClass();
-			JsonSerializer<Object> serializer;
-			if (PersistentMap.class.isAssignableFrom(clazz)) {
-				serializer = (JsonSerializer<Object>) buildMapSerializer(config, type, beanDesc, beanProperty);
-			} else {
-				serializer = (JsonSerializer<Object>) buildCollectionSerializer(config, type, beanDesc, beanProperty);
-			}
+//			Class<?> clazz = type.getRawClass();
+//			JsonSerializer<Object> serializer;
+//			if (PersistentMap.class.isAssignableFrom(clazz)) {
+//				serializer = (JsonSerializer<Object>) buildMapSerializer(config, type, beanDesc, beanProperty);
+//			} else {
+//				serializer = (JsonSerializer<Object>) buildCollectionSerializer(config, type, beanDesc, beanProperty);
+//			}
 			serializer.serialize(value, jgen, provider);
 		}
-	}
+
+
+    }
 
 	private class HibernateProxySerializer extends JsonSerializer<Object> {
 		private final JavaType type;
