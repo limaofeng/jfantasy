88c75ac8387da124e21ad0f3873a058d4692f3b0
limaofeng <limaofeng@msn.com>
2014-10-21 21:37:30 +0800

    将 jslib 移出 framework 库

--- jslib/static/js/common/formvalidator/themes/adminlogin/js/theme.js
+++
@@ -1,8 +0,0 @@
-var onShowHtml = '<span class="col999">$data$</span>';
-var onFocusHtml = '<span class="col999">$data$</span>';
-var onErrorHtml = '<span class="login_font_error">$data$</span>';
-var onCorrectHtml = '<span class="col999">$data$</span>';
-var onShowClass = '';
-var onFocusClass = '';
-var onErrorClass = '';
-var onCorrectClass = '';
--- jslib/static/js/fantasy/Fantasy.awt.SelectMenu.js
+++
@@ -1,9 +0,0 @@
-Fantasy.util.jClass(Fantasy.awt.Box,{
-
-	jClass : 'Fantasy.awt.SelectMenu',
-	
-	initialize : function($super,settings){
-		$super(settings);
-	}
-	
-});
