7df3112aa0eae49c78e1f1b76763b542428b2687
李茂峰 <limaofeng@msn.com>
2014-10-31 09:35:49 +0800

    websocket 代码修改

--- javaweb/JavaSource/com/haolue/websocket/WebSocketEndPoint.java
+++ javaweb/JavaSource/com/haolue/websocket/WebSocketEndPoint.java
@@ -8,6 +8,8 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class WebSocketEndPoint extends TextWebSocketHandler {
@@ -16,13 +18,25 @@ public class WebSocketEndPoint extends TextWebSocketHandler {
         System.out.println("WebSocketEndPoint");
     }
 
+    private List<WebSocketSession> sessions = new ArrayList<WebSocketSession>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+        sessions.add(session);
+    }
+
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         super.handleTextMessage(session, message);
         Principal principal = (Principal)OgnlUtil.getInstance().getValue("principal",session);
         UserDetails userDetails = principal instanceof Authentication ? (UserDetails) ((Authentication) principal).getPrincipal() : null;
         TextMessage returnMessage = new TextMessage(userDetails.getUsername() + " : " + message.getPayload() + " received at server");
-        session.sendMessage(returnMessage);
+        for(WebSocketSession _s : sessions) {
+            if(_s != session) {
+                _s.sendMessage(returnMessage);
+            }
+        }
     }
 
 }
