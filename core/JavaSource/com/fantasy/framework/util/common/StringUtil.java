7ce26bf74a2c659a17b25a8dc808b735ea0c2ee5
李茂峰 <limaofeng@msn.com>
2014-11-17 14:21:52 +0800

    代码优化

--- core/JavaSource/com/fantasy/framework/util/common/StringUtil.java
+++ core/JavaSource/com/fantasy/framework/util/common/StringUtil.java
@@ -11,15 +11,9 @@ import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.*;
 
-public class StringUtil {
+public abstract class StringUtil {
 
     private static final Log logger = LogFactory.getLog(StringUtil.class);
-
-    public static int PASSWORD_LEVEL_LOW = 1;
-
-    public static int PASSWORD_LEVEL_MIDDLE = 2;
-
-    public static int PASSWORD_LEVEL_HIGH = 3;
 
     /**
      * 字符串超出指定长度时截取到指定长度，并在末尾追加指定的字符串。
