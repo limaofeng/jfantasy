6e2276b34f64ab978ed367438f9d422fe7f4aa03
李茂峰 <limaofeng@msn.com>
2014-12-14 15:54:10 +0800

    修改编译错误

--- mall-ws-client/test/src/com/fantasy/mall/goods/ws/client/GoodsServiceTest.java
+++ mall-ws-client/test/src/com/fantasy/mall/goods/ws/client/GoodsServiceTest.java
@@ -2,10 +2,7 @@ package com.fantasy.mall.goods.ws.client;
 
 import com.fantasy.framework.ws.util.PagerDTO;
 import com.fantasy.framework.ws.util.PropertyFilterDTO;
-import com.fantasy.mall.goods.ws.dto.BrandDTO;
-import com.fantasy.mall.goods.ws.dto.BrandSeriesDTO;
-import com.fantasy.mall.goods.ws.dto.GoodsCategoryDTO;
-import com.fantasy.mall.goods.ws.dto.GoodsDTO;
+import com.fantasy.mall.goods.ws.dto.*;
 import com.fatnasy.ws.Constants;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,10 +13,7 @@ import java.util.List;
 
 public class GoodsServiceTest {
 
-
-
     private GoodsService goodsService;
-
 
     @Before
     public void init() throws Exception {
@@ -29,41 +23,21 @@ public class GoodsServiceTest {
         goodsService.afterPropertiesSet();
     }
 
-
-   // @Test
+    // @Test
     public void testFindPager() throws Exception {
         PagerDTO pager = new PagerDTO();// 设置每页显示的数据条数
-
-        //	pager.setPageSize(4);
-        //pager.setCurrentPage(1);
-        //pager.setOrderBy("releaseDate");
-
-
-
-
-
         // 设置过滤条件
         List<PropertyFilterDTO> filters = new ArrayList<PropertyFilterDTO>();
-        filters.add(new PropertyFilterDTO("EQS_category.sign","rental"));
-        //filters.add(new PropertyFilterDTO("BETWEENI_yachtlength","0-100"));
-        //filters.add(new PropertyFilterDTO("EQS_gdtype","jidongting"));
-        //filters.add(new PropertyFilterDTO("LIKES_brand.pinyin_OR_brand.engname","S"));
-        // filters.add(new PropertyFilterDTO("LIKES_brand.pinyin","S% "));
+        filters.add(new PropertyFilterDTO("EQS_category.sign", "rental"));
         // 调用接口查询
-        PagerDTO pagination = goodsService.findPager(pager, filters.toArray(new PropertyFilterDTO[filters.size()]));
+        GoodsPagerResult pagerResult = goodsService.findPager(pager, filters.toArray(new PropertyFilterDTO[filters.size()]));
         // PagerDTO pagination= goodsService.findPager(pager, filters.toArray(new PropertyFilterDTO[1]));
-        System.out.println("cccc"+pagination);
         // 打印结果
-        System.out.println(pagination.getPageItems());
-        for (GoodsDTO goodsDTO : pagination.getPageItems(new GoodsDTO[0])) {
-            System.out.println("aaaaaaaaaa"+goodsDTO.getDisabledTimes());
-
+        for (GoodsDTO goodsDTO : pagerResult.getPageItems()) {
 
         }
 
-
     }
-
 
 
     //@Test
@@ -77,40 +51,25 @@ public class GoodsServiceTest {
 
     //@Test
     public void testGetGoodsById() {
-        System.out.println("aa");
         Long id = Long.valueOf("82");
-        GoodsDTO pagination = goodsService.getGoodsById(id);
-        //System.out.println("aaaaaaaaaarr"+pagination.getCoverImage().getAbsolutePath());
-        //System.out.println("aaaaaaaaaarr"+pagination.getHarImage().getAbsolutePath());
-        // for (int i = 0; i < pagination.getDisabledTimes().length; i++) {
-        //	System.out.println("iiiiiiiiii"+ pagination.getDisabledTimes()[i].getTime());
-
-        //}
-        System.out.println("aa"+pagination.getSn());
-        //  System.out.println("aa"+pagination.getDisabledTimes().length);
-        //	System.out.println("ccccccccccrryyy"+pagination.getAddress());
+        GoodsDTO goodsDTO = goodsService.getGoodsById(id);
 
     }
 
     @Test
     public void testGetGoodsCategoryBySign() {
-        //GoodsCategoryDTO goodsCategoryDTO = goodsService.getGoodsCategoryBySign("found");
         GoodsCategoryDTO goodsCategoryDTO = goodsService.getGoodsCategoryBySign("yachtservice");
-        for (int i = 0; i < goodsCategoryDTO.getChildren().length; i ++) {
-            System.out.println("zzzzzzzzzzzzzzzzzz"+goodsCategoryDTO.getChildren()[i].getName());
+        for (int i = 0; i < goodsCategoryDTO.getChildren().length; i++) {
 
         }
 
     }
-   // @Test
-    public void testfind(){
 
+    public void testfind() {
         List<PropertyFilterDTO> filters = new ArrayList<PropertyFilterDTO>();
         filters.add(new PropertyFilterDTO("EQS_category.sign", "berth"));
         GoodsDTO[] atinfo = goodsService.find(filters.toArray(new PropertyFilterDTO[filters.size()]), null, null, 25);
-        System.out.println("result of atinfo: " + atinfo);
-        for (int i = 0; i < atinfo.length; i ++) {
-            System.out.println("iiiiiiiiii"+atinfo[i].getName());
+        for (int i = 0; i < atinfo.length; i++) {
 
         }
     }
