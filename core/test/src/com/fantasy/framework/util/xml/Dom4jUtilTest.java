c3f1f4b87784fb12329cc84bb9510feae5e92208
李茂峰 <limaofeng@msn.com>
2015-01-05 15:09:19 +0800

    "No runnable methods" bug 修复

--- core/test/src/com/fantasy/framework/util/xml/Dom4jUtilTest.java
+++ core/test/src/com/fantasy/framework/util/xml/Dom4jUtilTest.java
@@ -1,7 +1,6 @@
 package com.fantasy.framework.util.xml;
 
 import org.dom4j.Document;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,18 +12,8 @@ public class Dom4jUtilTest {
         document.accept(new Dom4jUtil.MyVistor()) ;
     }
 
-    @After
-    public void tearDown() throws Exception {
-
-    }
-
     @Test
     public void testReader() throws Exception {
-
-    }
-
-    @Test
-    public void testReader1() throws Exception {
 
     }
 
