1d85e24fd9b906d95439b0eef7ef018db3e5a60d
李茂峰 <limaofeng@msn.com>
2015-01-27 15:11:27 +0800

    组织机构相关修改

--- core/JavaSource/com/fantasy/security/bean/Job.java
+++ core/JavaSource/com/fantasy/security/bean/Job.java
@@ -1,84 +1,75 @@
 package com.fantasy.security.bean;
 
-import java.util.List;
+import com.fantasy.framework.dao.BaseBusEntity;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Transient;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import com.fantasy.framework.dao.BaseBusEntity;
 
 /**
  * 岗位
- * 
- * @功能描述
+ *
  * @author 李茂峰
- * @since 2013-1-22 下午04:00:48
  * @version 1.0
+ * @since 2013-1-22 下午04:00:48
  */
 @Entity
 @Table(name = "AUTH_JOB")
-@JsonIgnoreProperties( { "hibernateLazyInitializer" })
+@JsonIgnoreProperties({"hibernateLazyInitializer"})
 public class Job extends BaseBusEntity {
 
-	private static final long serialVersionUID = -7020427994563623645L;
+    private static final long serialVersionUID = -7020427994563623645L;
 
-	/**
-	 * 岗位简写
-	 */
-	@Id
-	@Column(name = "CODE")
-	private String code;
-	/**
-	 * 岗位名称
-	 */
-	@Column(name = "NAME")
-	private String name;
-	/**
-	 * 岗位描述信息
-	 */
-	@Column(name = "DESCRIPTION")
-	private String description;
-	/**
-	 * 所属组织机构
-	 */
-	@Transient
-	private List<Organization> organizations;
+    @Id
+    @Column(name = "ID")
+    private Long id;
+    /**
+     * 岗位简写
+     */
+    @Column(name = "CODE")
+    private String code;
+    /**
+     * 岗位名称
+     */
+    @Column(name = "NAME")
+    private String name;
+    /**
+     * 岗位描述信息
+     */
+    @Column(name = "DESCRIPTION")
+    private String description;
 
-	public String getCode() {
-		return code;
-	}
+    public String getCode() {
+        return code;
+    }
 
-	public void setCode(String code) {
-		this.code = code;
-	}
+    public void setCode(String code) {
+        this.code = code;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public List<Organization> getOrganizations() {
-		return organizations;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setOrganizations(List<Organization> organizations) {
-		this.organizations = organizations;
-	}
-
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
