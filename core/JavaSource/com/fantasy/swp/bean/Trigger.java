1b3ffa423bd11f568e564004ebb3fabd5a1067ec
李茂峰 <limaofeng@msn.com>
2015-02-05 12:00:53 +0800

    优化

--- core/JavaSource/com/fantasy/swp/bean/Trigger.java
+++ core/JavaSource/com/fantasy/swp/bean/Trigger.java
@@ -1,15 +1,16 @@
 package com.fantasy.swp.bean;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.fantasy.framework.dao.BaseBusEntity;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
 
 /**
  * 触发器
  */
 @Entity
 @Table(name = "SWP_TRIGGER")
-public class Trigger {
+public class Trigger extends BaseBusEntity {
 
     public static enum Type {
         /**
@@ -27,12 +28,25 @@ public class Trigger {
     }
 
     @Id
+    @Column(name = "ID", nullable = false, insertable = true, updatable = false, precision = 22, scale = 0)
+    @GeneratedValue(generator = "fantasy-sequence")
+    @GenericGenerator(name = "fantasy-sequence", strategy = "fantasy-sequence")
     private Long id;
-
+    /**
+     * 触发类型
+     */
+    @Enumerated(EnumType.STRING)
+    @Column(name = "NAME")
     private Type type;
-
+    /**
+     * 触发规则存储
+     */
+    @Column(name = "VALUE")
     private String value;
-
+    /**
+     * 触发器描述
+     */
+    private String description;
     /*
     Schedule editor
     1.Daily
@@ -40,4 +54,36 @@ public class Trigger {
     3.Days per month
     4.Cron expression
     */
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
