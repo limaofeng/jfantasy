b9f726fe8c62848431fb2f9c09076358ab045c9a
lishaozhe <616681695@qq.com>
2015-02-10 14:41:25 +0800

    岗位

--- core/JavaSource/com/fantasy/security/bean/Organization.java
+++ core/JavaSource/com/fantasy/security/bean/Organization.java
@@ -17,7 +17,7 @@ import java.util.List;
  */
 @Entity
 @Table(name = "AUTH_ORGANIZATION")
-@JsonIgnoreProperties({"hibernateLazyInitializer", "jobs"})
+@JsonIgnoreProperties({"hibernateLazyInitializer", "jobs","orgHelpBeans"})
 @org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Organization extends BaseBusEntity {
 
