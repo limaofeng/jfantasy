3e89e320a14f82b6cad96c93c981e4bebe13f2a8
何博 <hebo237x@163.com>
2015-03-25 16:44:03 +0800

    restful httpClient测试

--- javaweb/JavaSource/com/fantasy/web/ArticleAction.java
+++ javaweb/JavaSource/com/fantasy/web/ArticleAction.java
@@ -2,25 +2,36 @@ package com.fantasy.web;
 
 
 import com.fantasy.cms.bean.Article;
+import com.fantasy.framework.dao.Pager;
+import com.fantasy.framework.dao.hibernate.PropertyFilter;
 import com.fantasy.framework.struts2.ActionSupport;
+
+import java.util.List;
 
 public class ArticleAction extends ActionSupport {
 
-    public String index(){
+    public String search(Pager<Article> pager,List<PropertyFilter> filters){
         Article article = new Article();
         this.attrs.put(ROOT,new Article[]{article});
         return SUCCESS;
     }
 
-    public String create(){
+    public String create(Article article){
+        String title = article.getTitle();
+        this.attrs.put(ROOT,title);
         return SUCCESS;
     }
 
-    public String update(){
+    public String update(Article article){
+        String title = article.getTitle();
+        this.attrs.put(ROOT,title);
         return SUCCESS;
     }
 
-    public String destroy(){
+    public String delete(Long id){
+        Article article = new Article();
+        article.setId(id);
+        this.attrs.put(ROOT,article);
         return SUCCESS;
     }
 
