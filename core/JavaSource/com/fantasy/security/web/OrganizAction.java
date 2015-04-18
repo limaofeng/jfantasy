e5e5afcca85ac5fbcc86450e04256262aaed6ecf
何博 <hebo237x@163.com>
2015-04-18 15:43:20 +0800

    加上版本识别方法

--- core/JavaSource/com/fantasy/security/web/OrganizAction.java
+++ core/JavaSource/com/fantasy/security/web/OrganizAction.java
@@ -1,29 +1,36 @@
 package com.fantasy.security.web;
 
+import com.fantasy.attr.framework.util.VersionUtil;
 import com.fantasy.framework.dao.Pager;
 import com.fantasy.framework.dao.hibernate.PropertyFilter;
 import com.fantasy.framework.struts2.ActionSupport;
+import com.fantasy.framework.struts2.DynaModelDriven;
 import com.fantasy.security.bean.Organization;
 import com.fantasy.security.service.OrganizationService;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 组织机构Action
  *
  */
-public class OrganizAction extends ActionSupport {
+public class OrganizAction extends ActionSupport implements DynaModelDriven<Organization> {
 
     @Autowired
     private OrganizationService organizationService;
 
+    @Override
+    public Organization getModel(String methodName,String paramName,Class paramType,Map<String, Object> parameters) {
+        if("organization".equals(paramName) && Organization.class.isAssignableFrom(paramType)){
+            return VersionUtil.createDynaBean(Organization.class, "organization_v1");
+        }
+        return null;
+    }
 
     public String search(Pager<Organization> pager,List<PropertyFilter> filters){
-       /* if(ObjectUtil.find(filters, "filterName", "EQI_layer")==null) {
-            filters.add(new PropertyFilter("EQI_layer", "1"));
-        }*/
         this.attrs.put(ROOT,this.organizationService.findPager(pager,filters));
         return SUCCESS;
     }
