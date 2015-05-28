059f8cc289a5ab14274b3c749c2586f7374a1c7a
李茂峰 <limaofeng@msn.com>
2015-05-28 13:01:46 +0800

    去掉 job 的动态属性

--- core/JavaSource/com/fantasy/security/web/JobAction.java
+++ core/JavaSource/com/fantasy/security/web/JobAction.java
@@ -1,32 +1,21 @@
 package com.fantasy.security.web;
 
-import com.fantasy.attr.framework.util.VersionUtil;
 import com.fantasy.framework.dao.Pager;
 import com.fantasy.framework.dao.hibernate.PropertyFilter;
 import com.fantasy.framework.struts2.ActionSupport;
-import com.fantasy.framework.struts2.DynaModelDriven;
 import com.fantasy.security.bean.Job;
 import com.fantasy.security.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * 岗位Action
  */
-public class JobAction extends ActionSupport implements DynaModelDriven<Job> {
+public class JobAction extends ActionSupport {
 
     @Autowired
     private JobService jobService;
-
-    @Override
-    public Job getModel(String methodName,String paramName,Class paramType,Map<String, Object> parameters) {
-        if("job".equals(paramName) && Job.class.isAssignableFrom(paramType)){
-            return VersionUtil.createDynaBean(Job.class, "Job_v1");
-        }
-        return null;
-    }
 
     public String search(Pager<Job> pager,List<PropertyFilter> filters){
         this.attrs.put(ROOT,this.jobService.findPager(pager,filters));
