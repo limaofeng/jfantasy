ba11a7dd5a6e2d29094ad207d1b28fa425052dec
李茂峰 <limaofeng@msn.com>
2015-06-15 12:13:07 +0800

    部署测试修改

--- websocket/test/src/com/fantasy/spring/mvc/UserControllerTest.java
+++ websocket/test/src/com/fantasy/spring/mvc/UserControllerTest.java
@@ -6,7 +6,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,9 +17,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration(value = "src/main/webapp")
-@ContextHierarchy({
-        @ContextConfiguration(locations = "classpath:spring/applicationContext.xml")
-})
+@ContextConfiguration(locations = {"classpath:spring/applicationContext.xml"})
 public class UserControllerTest {
 
     private MockMvc mockMvc;
