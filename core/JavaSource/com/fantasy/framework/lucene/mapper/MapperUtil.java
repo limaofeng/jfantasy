af28296e4f95a35c978a5f7ccb128d0a2d8c0490
李茂峰 <limaofeng@msn.com>
2015-07-17 16:44:22 +0800

    FRAMEWORK-202 sonar 优化 Avoid commented-out lines of code 删除代码中的无用注释(多为方法备份)

--- core/JavaSource/com/fantasy/framework/lucene/mapper/MapperUtil.java
+++ core/JavaSource/com/fantasy/framework/lucene/mapper/MapperUtil.java
@@ -2,13 +2,8 @@ package com.fantasy.framework.lucene.mapper;
 
 public class MapperUtil {
 
-	public static String getEntityName(Class<?> clazz) {
-		// Entity entity = (Entity) clazz.getAnnotation(Entity.class);
-		String name = "";// entity.name();
-		if ("".equals(name)) {
-			name = clazz.getName().toLowerCase();
-		}
-		return name;
-	}
+    public static String getEntityName(Class<?> clazz) {
+        return clazz.getName().toLowerCase();
+    }
 
 }
