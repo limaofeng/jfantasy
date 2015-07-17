b6b58592a899e756831f2413b045baf4e71c077e
李茂峰 <limaofeng@msn.com>
2015-07-17 16:33:15 +0800

    FRAMEWORK-202 sonar 优化 Avoid commented-out lines of code 删除代码中的无用注释(多为方法备份)

--- core/JavaSource/com/fantasy/security/bean/handler/SexTypeHandler.java
+++ core/JavaSource/com/fantasy/security/bean/handler/SexTypeHandler.java
@@ -6,39 +6,36 @@ import com.fantasy.framework.dao.mybatis.type.CharEnumTypeHandler;
 import com.fantasy.security.bean.enums.Sex;
 
 public class SexTypeHandler extends CharEnumTypeHandler<Sex> {
-	public SexTypeHandler() {
-		super(new Converter() {
+    public SexTypeHandler() {
+        super(new Converter() {
 
-			@SuppressWarnings("rawtypes")
-			public Object convert(Class classes, Object value) {
-				if ((value instanceof Sex)) {
-					switch (((Sex) value).ordinal()) {
-					case 0:
-						return Character.valueOf('U');
-					case 1:
-						return Character.valueOf('M');
-					case 2:
-						return Character.valueOf('F');
-                    default:
+            @SuppressWarnings("rawtypes")
+            public Object convert(Class classes, Object value) {
+                if ((value instanceof Sex)) {
+                    switch (((Sex) value).ordinal()) {
+                        case 0:
+                            return 'U';
+                        case 1:
+                            return 'M';
+                        case 2:
+                            return 'F';
+                        default:
 
                     }
-					return Character.valueOf('U');
-				}
-				if ((value instanceof String)) {
-					switch (((String) value).charAt(0)) {
-					case 'F':
-						return Sex.female;
-					case 'M':
-						return Sex.male;
-                    default:
-						/*
-						 * case 'U': return Sex.unknown;
-						 */
-					}
-					/* return Sex.unknown; */
-				}
-				return null;
-			}
-		});
-	}
+                    return 'U';
+                }
+                if ((value instanceof String)) {
+                    switch (((String) value).charAt(0)) {
+                        case 'F':
+                            return Sex.female;
+                        case 'M':
+                            return Sex.male;
+                        default:
+                            return 'U';
+                    }
+                }
+                return 'U';
+            }
+        });
+    }
 }
