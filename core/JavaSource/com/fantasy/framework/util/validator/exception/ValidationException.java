a04b4be75695c7401c9a0a6336111a74dff497a5
李茂峰 <limaofeng@msn.com>
2015-07-27 16:33:00 +0800

    FRAMEWORK-202 sonar 优化 [ Tabulation characters should not be used ]

--- core/JavaSource/com/fantasy/framework/util/validator/exception/ValidationException.java
+++ core/JavaSource/com/fantasy/framework/util/validator/exception/ValidationException.java
@@ -4,17 +4,17 @@ import com.fantasy.framework.util.jackson.JSON;
 
 public class ValidationException extends Exception {
 
-	private static final long serialVersionUID = 3828944393587353554L;
+    private static final long serialVersionUID = 3828944393587353554L;
 
-	public ValidationException(String message) {
-		super(message);
-	}
+    public ValidationException(String message) {
+        super(message);
+    }
 
-	public String toString() {
-		if ((this instanceof StackValidationException)) {
-			StackValidationException stack = (StackValidationException) this;
-			return "ValidationException [message:" + getMessage() + ",errors:" + JSON.serialize(stack.errors) + "]";
-		}
-		return "ValidationException [" + JSON.serialize(this) + "]";
-	}
+    public String toString() {
+        if ((this instanceof StackValidationException)) {
+            StackValidationException stack = (StackValidationException) this;
+            return "ValidationException [message:" + getMessage() + ",errors:" + JSON.serialize(stack.errors) + "]";
+        }
+        return "ValidationException [" + JSON.serialize(this) + "]";
+    }
 }
