cde7382d516f7c8739604693285f86e032f0eb0a
李茂峰 <limaofeng@msn.com>
2015-07-27 15:38:09 +0800

    FRAMEWORK-202 sonar 优化 [ Case insensitive string comparisons should be made without intermediate upper or lower casing ]

--- core/JavaSource/com/fantasy/framework/dao/mybatis/dialect/DialectUtil.java
+++ core/JavaSource/com/fantasy/framework/dao/mybatis/dialect/DialectUtil.java
@@ -31,7 +31,7 @@ public class DialectUtil {
         @Override
         public String doReplace(String text, int index, Matcher matcher) {
             if (RegexpUtil.find(text.toLowerCase(), "^((asc)|(desc))$")) {
-                return "asc".equals(text.toLowerCase()) ? "desc" : "asc";
+                return "asc".equalsIgnoreCase(text) ? "desc" : "asc";
             }
             return text;
         }
