1522642def9c931f3b64b03e64b0f9de033baef4
何博 <hebo237x@163.com>
2015-08-13 15:52:58 +0800

    测试类报错修改

--- cms/test/src/com/fantasy/cms/rest/ArticleControllerTest.java
+++ cms/test/src/com/fantasy/cms/rest/ArticleControllerTest.java
@@ -1,9 +1,12 @@
 package com.fantasy.cms.rest;
 
 import com.fantasy.cms.bean.Article;
+import com.fantasy.cms.bean.ArticleCategory;
 import com.fantasy.cms.service.CmsService;
 import com.fantasy.framework.dao.Pager;
 import com.fantasy.framework.dao.hibernate.PropertyFilter;
+import com.fantasy.framework.error.IgnoreException;
+import com.fantasy.framework.util.common.JdbcUtil;
 import junit.framework.Assert;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -20,7 +23,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
@@ -44,31 +47,51 @@ public class ArticleControllerTest {
     @Before
     public void setUp() throws Exception {
         mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        //创建分类
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/cms/categorys").param("code","admintest").param("name","接口测试")).andDo(MockMvcResultHandlers.print()).andReturn();
+        Assert.assertEquals(200, result.getResponse().getStatus());
+        //创建文章
+        this.testSave();
     }
 
     @After
     public void tearDown() throws Exception {
-
+        ArticleCategory articleCategory = this.cmsService.get("admintest");
+        if(articleCategory!=null){
+            this.cmsService.delete(articleCategory.getCode());
+        }
     }
 
 
     @Test
-    @Transactional
     public void testFindPager() throws Exception {
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/cms/articles?pageSize=15&EQS_category.code=abcd3")).andDo(MockMvcResultHandlers.print()).andReturn();
+        MvcResult result = JdbcUtil.transaction(new JdbcUtil.Callback<MvcResult>() {
+            @Override
+            public MvcResult run() {
+                try {
+                    return mockMvc.perform(MockMvcRequestBuilders.get("/cms/articles?pageSize=15&EQS_category.code=admintest")).andDo(MockMvcResultHandlers.print()).andReturn();
+                } catch (Exception e) {
+                    throw new IgnoreException(e.getMessage(), e);
+                }
+            }
+        }, TransactionDefinition.PROPAGATION_REQUIRED);
         Assert.assertEquals(200, result.getResponse().getStatus());
     }
 
     @Test
-    @Transactional
     public void testView() throws Exception{
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/cms/articles/17")).andDo(MockMvcResultHandlers.print()).andReturn();
-        Assert.assertEquals(200, result.getResponse().getStatus());
+        List<PropertyFilter> filters = new ArrayList<PropertyFilter>();
+        filters.add(new PropertyFilter("EQS_title", "测试文章保存"));
+        List<Article> articleList = this.cmsService.findPager(new Pager<Article>(1),filters).getPageItems();
+        if(!articleList.isEmpty()){
+            MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/cms/articles/"+articleList.get(0).getId())).andDo(MockMvcResultHandlers.print()).andReturn();
+            Assert.assertEquals(200, result.getResponse().getStatus());
+        }
+
     }
 
-    @Test
     public void testSave() throws Exception{
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/cms/articles").param("title","测试文章保存").param("summary","保存").param("category.code","abcd3").param("content.text","测试文章保存测试文章保存测试文章保存测试文章保存测试文章保存测试文章保存")).andDo(MockMvcResultHandlers.print()).andReturn();
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/cms/articles").param("title","测试文章保存").param("summary","保存").param("category.code","admintest").param("content.text","测试文章保存测试文章保存测试文章保存测试文章保存测试文章保存测试文章保存")).andDo(MockMvcResultHandlers.print()).andReturn();
         Assert.assertEquals(200, result.getResponse().getStatus());
     }
 
@@ -78,23 +101,20 @@ public class ArticleControllerTest {
         filters.add(new PropertyFilter("EQS_title","测试文章保存"));
         List<Article> articleList = this.cmsService.findPager(new Pager<Article>(1),filters).getPageItems();
         if(!articleList.isEmpty()){
-            MvcResult result = mockMvc.perform(MockMvcRequestBuilders.put("/cms/articles/"+articleList.get(0).getId().toString()).param("summary","保存222")).andDo(MockMvcResultHandlers.print()).andReturn();
+            final String id = articleList.get(0).getId().toString();
+            MvcResult result = JdbcUtil.transaction(new JdbcUtil.Callback<MvcResult>() {
+                @Override
+                public MvcResult run() {
+                    try {
+                        return mockMvc.perform(MockMvcRequestBuilders.put("/cms/articles/"+id).param("summary","保存222")).andDo(MockMvcResultHandlers.print()).andReturn();
+                    } catch (Exception e) {
+                        throw new IgnoreException(e.getMessage(), e);
+                    }
+                }
+            }, TransactionDefinition.PROPAGATION_REQUIRED);
             Assert.assertEquals(200, result.getResponse().getStatus());
         }
     }
-
-
-    @Test
-    public void testbatchDelete() throws Exception{
-        List<PropertyFilter> filters = new ArrayList<PropertyFilter>();
-        filters.add(new PropertyFilter("EQS_title","测试文章保存"));
-        List<Article> articleList = this.cmsService.findPager(new Pager<Article>(2),filters).getPageItems();
-        if(!articleList.isEmpty() && articleList.size()==2){
-            MvcResult result = mockMvc.perform(MockMvcRequestBuilders.delete("/cms/articles").param("id",articleList.get(0).getId().toString()).param("id",articleList.get(1).getId().toString())).andDo(MockMvcResultHandlers.print()).andReturn();
-            Assert.assertEquals(200, result.getResponse().getStatus());
-        }
-    }
-
 
     @Test
     public void testDelete() throws Exception{
