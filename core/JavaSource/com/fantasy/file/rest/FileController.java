f3b06484947503216404425330610d044ec80609
李茂峰 <limaofeng@msn.com>
2015-08-31 15:32:52 +0800

    添加 cors-filter 过滤器

--- core/JavaSource/com/fantasy/file/rest/FileController.java
+++ core/JavaSource/com/fantasy/file/rest/FileController.java
@@ -8,15 +8,11 @@ import com.fantasy.file.service.FilePartService;
 import com.fantasy.file.service.FileService;
 import com.fantasy.file.service.FileUploadService;
 import com.fantasy.framework.util.common.ObjectUtil;
-import com.fantasy.framework.util.jackson.JSON;
-import com.fantasy.framework.util.web.WebUtil;
-import com.fantasy.framework.util.web.context.ActionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -45,17 +41,18 @@ public class FileController {
      * @throws IOException
      */
     @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
     public FileDetail upload(@RequestParam(value = "attach", required = false) MultipartFile file, String dir, String entireFileName, String entireFileDir, String entireFileHash, String partFileHash, Integer total, Integer index) throws IOException {
-        FileDetail fileDetail = fileUploadService.upload(file, dir, entireFileName, entireFileDir, entireFileHash, partFileHash, total, index);
-        if (WebUtil.browser(ActionContext.getContext().getHttpRequest()) == WebUtil.Browser.msie) {
-            PrintWriter writer = ActionContext.getContext().getHttpResponse().getWriter();
-            writer.print(JSON.serialize(fileDetail));
-            writer.flush();
-        }
-        return fileDetail;
+        return fileUploadService.upload(file, dir, entireFileName, entireFileDir, entireFileHash, partFileHash, total, index);
+//        if (WebUtil.browser(ActionContext.getContext().getHttpRequest()) == WebUtil.Browser.msie) {
+//            PrintWriter writer = ActionContext.getContext().getHttpResponse().getWriter();
+//            writer.print(JSON.serialize(fileDetail));
+//            writer.flush();
+//        }
     }
 
     @RequestMapping(value = "/{hash}/pass", method = RequestMethod.GET)
+    @ResponseBody
     public Map<String, Object> pass(@PathVariable("hash") String hash) {
         List<FilePart> parts = filePartService.find(hash);
         Map<String, Object> data = new HashMap<String, Object>();
