e4c8bf59ebb81728727e5a91038ae4fc3b8eba86
李茂峰 <limaofeng@msn.com>
2015-09-24 12:20:25 +0800

    修改 MySwaggerConfig 配置

--- core/JavaSource/com/fantasy/framework/swagger/SpringfoxSwaggerConfig.java
+++ core/JavaSource/com/fantasy/framework/swagger/SpringfoxSwaggerConfig.java
@@ -1,38 +1,25 @@
 package com.fantasy.framework.swagger;
 
 import com.fantasy.framework.util.common.PropertiesHelper;
-import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static springfox.documentation.builders.PathSelectors.regex;
-import static com.google.common.base.Predicates.or;
 
 @Configuration
 @EnableSwagger2
-public class MySwaggerConfig {
+public class SpringfoxSwaggerConfig {
 
     @Bean
     public Docket swaggerSpringMvcPlugin() {
         return new Docket(DocumentationType.SWAGGER_2).select().build().apiInfo(apiInfo());//.paths(paths())
     }
 
-    private Predicate<String> paths() {
-        return or(
-                regex("/business.*"),
-                regex("/some.*"),
-                regex("/contacts.*"),
-                regex("/pet.*"),
-                regex("/springsRestController.*"),
-                regex("/test.*"));
-    }
-
     private ApiInfo apiInfo;
 
-    public MySwaggerConfig() {
+    public SpringfoxSwaggerConfig() {
         PropertiesHelper propertiesHelper = PropertiesHelper.load("props/application.properties");
         apiInfo = new ApiInfo(
                 propertiesHelper.getProperty("swagger.api.title", "My Apps API Title"),
