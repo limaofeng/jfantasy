2063b46834597f7bf87e064c79215c04b967852a
李茂峰 <limaofeng@msn.com>
2016-08-19 16:07:48 +0800

    捕获异常,避免加载失败

--- pay-client/JavaSource/org/jfantasy/pay/order/OrderServiceRegistryRunner.java
+++ pay-client/JavaSource/org/jfantasy/pay/order/OrderServiceRegistryRunner.java
@@ -1,6 +1,8 @@
 package org.jfantasy.pay.order;
 
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.jfantasy.rpc.config.NettyLocalSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceRegistryRunner implements CommandLineRunner {
+
+    public static final Log LOG = LogFactory.getLog(OrderServiceRegistryRunner.class);
 
     @Autowired
     public NettyLocalSettings nettyLocalSettings;
@@ -20,7 +24,11 @@ public class OrderServiceRegistryRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        orderServiceRegistry.register(orderService.types(), nettyLocalSettings.getHost(), nettyLocalSettings.getPort());
+        try {
+            orderServiceRegistry.register(orderService.types(), nettyLocalSettings.getHost(), nettyLocalSettings.getPort());
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
     }
 
 }
