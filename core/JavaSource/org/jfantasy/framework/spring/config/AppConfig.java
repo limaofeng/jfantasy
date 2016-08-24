752bc443b709bcf7aee63b280ee5e005f7b9e39e
李茂峰 <limaofeng@msn.com>
2016-08-24 16:25:49 +0800

    添加 EnableAsync

--- core/JavaSource/org/jfantasy/framework/spring/config/AppConfig.java
+++ core/JavaSource/org/jfantasy/framework/spring/config/AppConfig.java
@@ -6,6 +6,8 @@ import org.jfantasy.framework.lucene.BuguIndex;
 import org.jfantasy.framework.util.common.ClassUtil;
 import org.jfantasy.framework.util.common.PropertiesHelper;
 import org.jfantasy.framework.util.common.StringUtil;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +15,11 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.scheduling.SchedulingTaskExecutor;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 /**
  * Description: <应用配置类>. <br>
@@ -23,6 +29,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
  */
 @Configuration
 @EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableAsync
 @Import({DaoConfig.class, QuartzConfig.class})
 public class AppConfig {
 
@@ -69,17 +76,38 @@ public class AppConfig {
 
     @Bean(name = "taskExecutor")
     public SchedulingTaskExecutor taskExecutor() {
-        ThreadPoolTaskExecutor poolTaskExecutor = new ThreadPoolTaskExecutor();
+        ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
         //线程池所使用的缓冲队列
-        poolTaskExecutor.setQueueCapacity(200);
+        pool.setQueueCapacity(200);
         //线程池维护线程的最少数量
-        poolTaskExecutor.setCorePoolSize(5);
+        pool.setCorePoolSize(5);
         //线程池维护线程的最大数量
-        poolTaskExecutor.setMaxPoolSize(1000);
+        pool.setMaxPoolSize(1000);
         //线程池维护线程所允许的空闲时间
-        poolTaskExecutor.setKeepAliveSeconds(30000);
-        poolTaskExecutor.initialize();
-        return poolTaskExecutor;
+        pool.setKeepAliveSeconds(30000);
+        pool.setThreadNamePrefix("Task-");
+        pool.initialize();
+        return pool;
+    }
+
+    @Configuration
+    public static class ThreadingConfig implements AsyncConfigurer {
+
+        @Override
+        public Executor getAsyncExecutor() {
+            ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
+            pool.setMaxPoolSize(10);
+            pool.setCorePoolSize(10);
+            pool.setThreadNamePrefix("Spring-Async-");
+            pool.initialize();
+            return pool;
+        }
+
+        @Override
+        public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+            return new SimpleAsyncUncaughtExceptionHandler();
+        }
+
     }
 
     /*
@@ -100,7 +128,7 @@ public class AppConfig {
         }
         buguIndex.setBasePackage(StringUtil.join(helper.getMergeProperty("indexes.scan.package"), ";"));
         buguIndex.setDirectoryPath(helper.getProperty("indexes.storage.path"));
-        buguIndex.setIndexReopenPeriod(helper.getLong("indexes.reopen.reriod",30000L));
+        buguIndex.setIndexReopenPeriod(helper.getLong("indexes.reopen.reriod", 30000L));
         buguIndex.setExecutor(taskExecutor());
         buguIndex.setRebuild(helper.getBoolean("indexes.rebuild", false));
         return buguIndex;
