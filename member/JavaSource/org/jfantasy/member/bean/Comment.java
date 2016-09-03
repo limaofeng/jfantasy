7684be0c44edfee985e9a5aa7c34837faf46f0e3
李茂峰 <limaofeng@msn.com>
2016-09-03 13:33:34 +0800

    评论添加动态字段

--- member/JavaSource/org/jfantasy/member/bean/Comment.java
+++ member/JavaSource/org/jfantasy/member/bean/Comment.java
@@ -1,15 +1,20 @@
 package org.jfantasy.member.bean;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import org.hibernate.annotations.GenericGenerator;
 import org.jfantasy.framework.dao.BaseBusEntity;
+import org.jfantasy.framework.dao.hibernate.converter.PropertiesConverter;
+import org.jfantasy.framework.jackson.ThreadJacksonMixInHolder;
 import org.jfantasy.framework.spring.validation.RESTful;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * 评论表
@@ -78,6 +83,13 @@ public class Comment extends BaseBusEntity {
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
     @JoinColumn(name = "MEMBER_ID", updatable = false, nullable = false, foreignKey = @ForeignKey(name = "FK_SHIP_ADDRESS_MEMBER"))
     private Member member;
+    /**
+     * 扩展属性
+     */
+    @ApiModelProperty(hidden = true)
+    @Convert(converter = PropertiesConverter.class)
+    @Column(name = "PROPERTIES", columnDefinition = "Text")
+    private Properties properties;
 
     public Long getId() {
         return id;
@@ -183,6 +195,29 @@ public class Comment extends BaseBusEntity {
 
     public void setWith(String with) {
         this.with = with;
+    }
+
+    @JsonAnySetter
+    public void set(String key, Object value) {
+        if (value == null) {
+            return;
+        }
+        if (this.properties == null) {
+            this.properties = new Properties();
+        }
+        this.properties.put(key, value);
+    }
+
+    @JsonAnyGetter
+    public Properties getProperties() {
+        if (ThreadJacksonMixInHolder.getMixInHolder().isIgnoreProperty(Invite.class, "properties")) {
+            return null;
+        }
+        return properties;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
     }
 
 }
