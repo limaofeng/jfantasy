74e1a316b40e107e21e07bb3cdbe720e19667420
李茂峰 <limaofeng@msn.com>
2016-09-22 17:00:25 +0800

    /teams/{id}/members  返回数据调整

--- member/JavaSource/org/jfantasy/member/rest/TeamController.java
+++ member/JavaSource/org/jfantasy/member/rest/TeamController.java
@@ -2,6 +2,8 @@ package org.jfantasy.member.rest;
 
 import org.jfantasy.framework.dao.Pager;
 import org.jfantasy.framework.dao.hibernate.PropertyFilter;
+import org.jfantasy.framework.jackson.annotation.IgnoreProperty;
+import org.jfantasy.framework.jackson.annotation.JsonResultFilter;
 import org.jfantasy.framework.spring.mvc.error.NotFoundException;
 import org.jfantasy.framework.spring.mvc.hateoas.ResultResourceSupport;
 import org.jfantasy.framework.spring.validation.RESTful;
@@ -20,7 +22,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-/** 团队 **/
+/**
+ * 团队
+ **/
 @RestController
 @RequestMapping("/teams")
 public class TeamController {
@@ -36,7 +40,9 @@ public class TeamController {
     @Autowired
     private AddressController addressController;
 
-    /** 团队列表 - 团队列表 **/
+    /**
+     * 团队列表 - 团队列表
+     **/
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
     public Pager<ResultResourceSupport> search(@RequestParam(value = "type", required = false) String type, Pager<Team> pager, List<PropertyFilter> filters) {
@@ -44,26 +50,34 @@ public class TeamController {
         return assembler.toResources(this.teamService.findPager(pager, filters));
     }
 
-    /** 查看团队 - 查看团队 **/
+    /**
+     * 查看团队 - 查看团队
+     **/
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResultResourceSupport view(@PathVariable("id") String id) {
         return assembler.toResource(this.get(id));
     }
 
-    /** 添加团队 - 添加团队 **/
+    /**
+     * 添加团队 - 添加团队
+     **/
     @RequestMapping(method = RequestMethod.POST)
     public ResultResourceSupport create(@Validated(RESTful.POST.class) @RequestBody Team team) {
         return assembler.toResource(this.teamService.save(team));
     }
 
-    /** 更新团队 - 更新团队地址 **/
+    /**
+     * 更新团队 - 更新团队地址
+     **/
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResultResourceSupport update(@PathVariable("id") String id, @RequestBody Team team) {
         team.setKey(id);
         return assembler.toResource(this.teamService.update(team));
     }
 
-    /** 删除团队 - 删除团队 **/
+    /**
+     * 删除团队 - 删除团队
+     **/
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable("id") String id) {
@@ -71,7 +85,9 @@ public class TeamController {
     }
 
 
-    /** 邀请列表 - 邀请列表 **/
+    /**
+     * 邀请列表 - 邀请列表
+     **/
     @RequestMapping(value = "/{id}/invites", method = RequestMethod.GET)
     @ResponseBody
     public Pager<ResultResourceSupport> invites(@PathVariable("id") String id, Pager<Invite> pager, List<PropertyFilter> filters) {
@@ -79,22 +95,34 @@ public class TeamController {
         return InviteController.assembler.toResources(this.inviteService.findPager(pager, filters));
     }
 
-    /** 批量邀请 - 批量邀请 **/
+    /**
+     * 批量邀请 - 批量邀请
+     **/
     @RequestMapping(value = "/{id}/invites", method = RequestMethod.POST)
     @ResponseBody
     public List<ResultResourceSupport> invites(@PathVariable("id") String id, @RequestBody List<Invite> invites) {
         return InviteController.assembler.toResources(inviteService.save(id, invites));
     }
 
-    /** 团队成员列表 **/
-    @RequestMapping(value = "/{id}/member", method = RequestMethod.GET)
+    /**
+     * 团队成员列表
+     *
+     * @param id      集团ID
+     * @param pager   翻页对象
+     * @param filters 过滤接口
+     * @return Pager<ResultResourceSupport>
+     */
+    @JsonResultFilter(ignore = @IgnoreProperty(pojo = TeamMember.class, name = {"team", "member"}))
+    @RequestMapping(value = "/{id}/members", method = RequestMethod.GET)
     @ResponseBody
     public Pager<ResultResourceSupport> members(@PathVariable("id") String id, Pager<TeamMember> pager, List<PropertyFilter> filters) {
         filters.add(new PropertyFilter("EQS_team.key", id));
         return TeamMemberController.assembler.toResources(this.teamMemberService.findPager(pager, filters));
     }
 
-    /** 团队地址列表 **/
+    /**
+     * 团队地址列表
+     **/
     @RequestMapping(value = "/{id}/addresses", method = RequestMethod.GET)
     @ResponseBody
     public List<Address> addresses(@PathVariable("id") String id, Pager<Address> pager, List<PropertyFilter> filters) {
